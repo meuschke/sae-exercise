860e3ed6911c26ab5f532ab5888b1a7dcfa360b5
Toni Meuschke <toni.meuschke@googlemail.com>
2019-03-11 14:50:33 +0100

    Add mongo

--- 01-SQL-injection/mongodb.js
+++ 01-SQL-injection/mongodb.js
@@ -1,19 +1,23 @@
-var mysql = require("mysql");
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express')
 const app = express()
 const port = 3000
 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'myproject';
+
 //Database connection
 app.use(function(req, res, next){
-	res.locals.connection = mysql.createConnection({
-		host     : 'localhost',
-		user     : 'root',
-		password : '123456',
-		database : 'test',
-		multipleStatements: true
+	// Use connect method to connect to the server
+	MongoClient.connect(url, function(err, client) {
+	  console.log("Connected successfully to mongo");
+
+	  const db = client.db(dbName);
+		res.locals.db = db
+	 	next();
 	});
-	res.locals.connection.connect();
-	next();
 });
 
 app.get('/', (req, res) => res.send('Hello World!'))
@@ -21,10 +25,14 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/sql-injection', function(req, res, next) {
 	console.log(req.query.id)
-	res.locals.connection.query('Select * from users WHERE id =' + req.query.id, function (error, results, fields) {
-		if (error) throw error;
-		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-	});
+	var query = {
+         id: req.query.id
+     }
+
+   res.locals.db.collection('users').findOne(query, function (error, user) {
+		 if (error) throw error;
+		 res.send(JSON.stringify({"status": 200, "error": null, "response": user}));
+   });
 });
 
 app.listen(port, () => console.log(`Example app listening on http://localhost:${port}!`))
